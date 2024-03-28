3b316640bbe1e8897c2d0d76b88c13c62815828a
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-03-29 02:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	if(num == 2){
+		return 1;
+	}
+	if(num == 1){
+		return 0;
+	}
+	int fib = fibonacci(num-1) + fibonacci(num-2);
+	return fib;
 }
+
 
 module.exports = fibonacci;
